55f9ad95971310d1db69d5401ca20ab71045c01f
Björn (^_^) Engqvist <loloolllool@lolcathost.se>
2023-12-13 22:32:40 +0000

    example project

---
+++ src/src/App.js
@@ -0,0 +1,15 @@
+import logo from './dockerlove.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Tack för er tid :)</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
